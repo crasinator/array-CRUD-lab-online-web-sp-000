70575fccf01953d9d1f7760e1ced3e389f88f43a
Jordan Reed <jordanreed03@gmail.com>
2020-11-26 18:04:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,17 +11,18 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array elenent
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-
+array.pop()
   
 end
 
 def remove_element_from_start_of_array(array)
+array.shift()  
 end
-end
+
 
 def retrieve_element_from_index(array, index_number)
   
