68b8470b7affe0e0a5143f828a74b92bec438de3
Jordan Reed <jordanreed03@gmail.com>
2020-11-26 17:50:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array1 = ["ween", "ween", "weeny"] 
+  array1 = ["ween", "ween", "weeny", "weeeeeeeeee"] 
 end
 
 def add_element_to_end_of_array(array, element)
